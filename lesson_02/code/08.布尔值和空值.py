39a2932fa659112a1920ebababc602382b90e935
chenpi <641760512@qq.com>
2022-05-12 17:39:25 +0800

    基础类型转换

---
+++ lesson_02/code/08.布尔值和空值.py
@@ -0,0 +1,13 @@
+# 布尔值（Boolean 简写 bool）
+# 用来做逻辑判断  True和False
+a = True
+a = False
+print('a =', a)
+
+# 布尔值实际上也属于整型，True相当于1，False相当于0
+print(1 + a)
+
+# None (空值)
+# None专门来表示不存在
+b = None
+print(b)
---
+++ lesson_02/code/09.类型检查.py
@@ -0,0 +1,11 @@
+# 通过类型检查，可以检查变量的类型
+a = 123
+b = 12.3
+c = "123"
+d = True
+e = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
