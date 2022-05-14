998663aa11af4c390966366ca1126c890d09fb45
wlf <w-852123@126.com>
2022-05-14 14:20:11 +0800

    lesson_04 通用操作

---
+++ lesson_04/code/03.通用操作.py
@@ -0,0 +1,8 @@
+names = ["tom", "jack", "amy"]
+print(names + ["helloworld"])
+print(names * 2)
+
+print("tom" in names)
+print("tom" not in names)
+print("jerry" in names)
+print("jerry" not in names)
